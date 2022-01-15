7dd505dfdb9c8fa306c8093e7fd17df45a334a34
Marco Clarindo <marcos_clarindo@hotmail.com>
2022-01-14 21:11:56 -0300

    Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,25 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start (Stage primatyStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			Scene mainScene = new Scene(parent);
+			primatyStage.setScene(mainScene);
+			primatyStage.setTitle("Sample JavaFX application");
+			primatyStage.show();
+		} 
+	    catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
